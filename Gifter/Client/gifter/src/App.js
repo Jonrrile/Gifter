3cfe757705fb11b0ccf2a7aed53ab4879544bc14
Jonathan Riley <Jonrrile@ut.utm.edu>
2020-10-02 15:46:58 -0500

    search bar in progress

--- Gifter/Client/gifter/src/App.js
+++ Gifter/Client/gifter/src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <PostProvider>
+        <input type="search" placeholder="Search Posts" />
         <PostForm />
         <PostList />
       </PostProvider>
